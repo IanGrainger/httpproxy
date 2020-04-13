278e508e71753ab66c8ad8a4f28d200c72723299
Ian Grainger <ian.grainger@gmail.com>
2020-04-13 18:03:48 +0100

    don't re-parse the json

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const axios = require("axios").default;
 
 module.exports = async (req, res) => {
   console.log("req.body", req.body);
-  const { url, query, method } = JSON.parse(req.body);
+  const { url, query, method } = req.body;
   //ignoring query!
   const methodResult = await axios.request({
     method: method,
@@ -10,6 +10,7 @@ module.exports = async (req, res) => {
     responseType: "json",
   });
 
+  console.log("got method result", methodResult);
   res
     .status(200)
     .send(
