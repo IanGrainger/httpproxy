1d9487ed3a273cb25f725d356b824948b71f5474
Ian Grainger <ian.grainger@gmail.com>
2020-04-13 15:11:22 +0100

    use axios to make request and return result

--- api/index.js
+++ api/index.js
@@ -1,30 +1,19 @@
-const https = require("https");
+const axios = require("axios").default;
 
-// https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
-//   let data = '';
-
-//   // A chunk of data has been recieved.
-//   resp.on('data', (chunk) => {
-//     data += chunk;
-//   });
-
-//   // The whole response has been received. Print out the result.
-//   resp.on('end', () => {
-//     console.log(JSON.parse(data).explanation);
-//   });
-
-// }).on("error", (err) => {
-//   console.log("Error: " + err.message);
-// });
-
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const { name = "World" } = req.query;
 
   const { url, query, method } = JSON.parse(req.body);
+  //ignoring query!
+  const methodResult = await axios.request({
+    method: method,
+    url,
+    responseType: "json",
+  });
 
   res
     .status(200)
     .send(
-      `Hello ${name}!\nbody ${req.body}\nI got these params: ${url} ${query} ${method}`
+      `Hello ${name}!\nbody ${req.body}\nI got these params: ${url} ${query} ${method}\nAnd this result ${methodResult}`
     );
 };
